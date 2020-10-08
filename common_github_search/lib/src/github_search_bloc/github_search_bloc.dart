f6e89959c0f2d7d3b232ed6472e5880817406e61
kardihaekal <kardyhaekal@gmail.com>
2020-10-08 16:18:48 +0800

    first commit

---
+++ common_github_search/lib/src/github_search_bloc/github_search_bloc.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:meta/meta.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:bloc/bloc.dart';
+
+import 'package:common_github_search/common_github_search.dart';
+
+class GithubSearchBloc extends Bloc<GithubSearchEvent, GithubSearchState> {
+  final GithubRepository githubRepository;
+
+  GithubSearchBloc({@required this.githubRepository})
+      : super(SearchStateEmpty());
+
+  @override
+  Stream<Transition<GithubSearchEvent, GithubSearchState>> transformEvents(
+    Stream<GithubSearchEvent> events,
+    Stream<Transition<GithubSearchEvent, GithubSearchState>> Function(
+      GithubSearchEvent event,
+    )
+        transitionFn,
+  ) {
+    return events
+        .debounceTime(const Duration(milliseconds: 300))
+        .switchMap(transitionFn);
+  }
+
+  @override
+  Stream<GithubSearchState> mapEventToState(GithubSearchEvent event) async* {
+    if (event is TextChanged) {
+      final String searchTerm = event.text;
+      if (searchTerm.isEmpty) {
+        yield SearchStateEmpty();
+      } else {
+        yield SearchStateLoading();
+        try {
+          await Future<void>.delayed(Duration(seconds: 2));
+          final results = await githubRepository.search(searchTerm);
+          yield SearchStateSuccess(results.items);
+        } catch (error) {
+          yield error is SearchResultError
+              ? SearchStateError(error.message)
+              : SearchStateError('something went wrong');
+        }
+      }
+    }
+  }
+}
---
+++ flutter_bloc_github_search/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_github_search/search_form.dart';
+
+import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:common_github_search/common_github_search.dart';
+
+void main() {
+  final GithubRepository _githubRepository = GithubRepository(
+    GithubCache(),
+    GithubClient(),
+  );
+
+  runApp(App(githubRepository: _githubRepository));
+}
+
+class App extends StatelessWidget {
+  final GithubRepository githubRepository;
+
+  const App({
+    Key key,
+    @required this.githubRepository,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Github Search',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Github Search')),
+        body: BlocProvider(
+          create: (context) =>
+              GithubSearchBloc(githubRepository: githubRepository),
+          child: SearchForm(),
+        ),
+      ),
+    );
+  }
+}
